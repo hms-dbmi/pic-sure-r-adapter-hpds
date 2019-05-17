2c7a553b4919ef582fca744b687d98f0098f4a2d
Nick Benik <nbenik@gmail.com>
2019-05-16 20:02:44 -0400

    Alpha Release

--- R/PicSureHpds.R
+++ R/PicSureHpds.R
@@ -6,7 +6,7 @@ Adapter <- R6::R6Class("PicSureHpdsAdapter",
                                   lock_objects = FALSE,
                                   public = list(
                                     initialize = function(PicSureConnection) {
-                                      self$connection <- PicSureConnection
+                                      self$connection_reference <- PicSureConnection
                                     },
                                     help = function() {
                                       cat("        [HELP] PicSureHpdsLib::Adapter$new(picsure_connection)\n")
@@ -16,11 +16,15 @@ Adapter <- R6::R6Class("PicSureHpdsAdapter",
                                       invisible(self)
                                     },
                                     version = function() {
+                                      cat(paste("PicSureHpdsLib Library (version ", packageVersion("PicSureHpdsLib"), ")\n", sep=""))
+                                      cat(paste("URL: ", self$connection_reference$url, "\n", sep=""))
                                       invisible(self)
                                     },
-                                    list = function() {},
+                                    list = function() {
+                                      self$connection_reference$list()
+                                    },
                                     useResource = function(resource_uuid) {
-                                      return(PicSureHpdsResourceConnection$new(self$connection, resource_uuid))
+                                      return(PicSureHpdsResourceConnection$new(self$connection_reference, resource_uuid))
                                     }
                                   )
 )
@@ -40,12 +44,12 @@ PicSureHpdsResourceConnection <- R6::R6Class("PicSureHpdsResourceConnection",
                                       }
                                     },
                                     help = function() {
-                                      cat("
-        [HELP] PicSureHpdsLib::Adapter$useResource(resource_uuid)
-            $dictionary()       Used to access data dictionary of the resource
-            $query()            Used to query against data in the resource
-            
-        [ENVIRONMENT]")
+                                      cat("        [HELP] PicSureHpdsLib::Adapter$useResource(resource_uuid)\n")
+                                      cat("            $dictionary()       Used to access data dictionary of the resource\n")
+                                      cat("            $query()            Used to query against data in the resource\n")
+                                      cat("        [ENVIRONMENT]\n")
+                                      cat(paste("            URL: \n", self$connection_reference$url, sep=""))
+                                      cat(paste("  Resource UUID: \n", self$resourceUUID, sep=""))
                                     },
                                     dictionary = function() {
                                       return(PicSureHpdsDictionary$new(self))
@@ -57,17 +61,11 @@ PicSureHpdsResourceConnection <- R6::R6Class("PicSureHpdsResourceConnection",
 )
 
 
-
-
-
-
-
-
-
 #' export
 BypassAdapter <- R6::R6Class("PicSureHpdsBypassAdapter",
                                         portable = FALSE,
                                         lock_objects = FALSE,
+                                        inherit = Adapter,
                                         public = list(
                                           initialize = function(url_arg, token_arg) {
                                             # trim and make sure URL ends in "/"
@@ -87,34 +85,63 @@ BypassAdapter <- R6::R6Class("PicSureHpdsBypassAdapter",
                                             }
                                             return(temp)
                                           },
+                                          list = function() {
+                                            self$connection_reference$list()
+                                          },
                                           help = function() {
-                                            cat("
-        [HELP] PicSureHpdsLib::BypassAdapter$new(HPDS_Url, Security_Token)
-            $version()                      gives version information for library
-            $list()                         lists available resources
-            $useResource(resource_uuid)     returns an object for selected resource
-                                            ")
+                                            cat("        [HELP] PicSureHpdsLib::BypassAdapter$new(HPDS_Url, Security_Token)\n")
+                                            cat("            $version()                      gives version information for library\n")
+                                            cat("            $list()                         lists available resources\n")
+                                            cat("            $useResource(resource_uuid)     returns an object for selected resource\n")
                                           }
                                         )
 )
+
 
 #' export
 PicSureHpdsBypassConnection <- R6::R6Class("PicSureHpdsBypassConnection",
                                            portable = FALSE,
                                            lock_objects = FALSE,
+                                           private = list(
+                                           ),
                                            public = list(
                                              initialize = function(url_arg, token_arg) {
                                                self$url <- url_arg
                                                self$token <- token_arg
                                              },
+                                             list = function() {
+                                               res = self$getResources()
+                                               if (nrow(res) > 0) {
+                                                 print(paste(DescTools::StrPad("+", width=39, pad="-"), DescTools::StrPad("+", width=55, pad="-"), sep=""))
+                                                 print(paste(DescTools::StrPad("| Resource UUID", width=38, pad=" "), DescTools::StrPad("| Resource Name", width=55, pad=" "), sep=""))
+                                                 print(paste(DescTools::StrPad("+", width=39, pad="-"), DescTools::StrPad("+", width=55, pad="-"), sep=""))
+                                                 for (idx in 1:nrow(res)) {
+                                                   print(paste(DescTools::StrPad(paste("|", res$uuid[[idx]], sep=""), width=34, pad=" "), "| ", res$name[[idx]]))
+                                                   print(paste("| Description: ", res$description[[idx]], sep=""))
+                                                   print(paste(DescTools::StrPad("+", width=39, pad="-"), DescTools::StrPad("+", width=55, pad="-"), sep=""))
+                                                 }
+                                               }
+                                             },
                                              INTERNAL_api_obj = function() {
                                                return(PicSureHpdsBypassConnectionAPI$new(self$url, self$token))
                                              },
+                                             getResources = function() {
+                                               full_url = paste(self$url, "info", sep="")
+                                               request = POST(full_url, body="{}", content_type_json(), add_headers(Authorization=paste('Bearer',self$token)))
+                                               if (request$status_code != 200) { 
+                                                 print("ERROR: HTTP response was bad")
+                                                 print(request)
+                                                 return(jsonlite::fromJSON('[]')) 
+                                               } else {
+                                                 results = content(request, "parsed")
+                                                 ret = paste('[{"uuid":"', results[["id"]], '", "name":"', results[["name"]], '", "description":"[Resource accessed directly (bypassing PIC-SURE framework)]"}]')
+                                                 return(jsonlite::fromJSON(ret))
+                                               }
+                                             },
                                              help = function() {
-                                               cat("
-        [HELP] PicSureHpdsLib::PicSureHpdsBypassConnection(url, token)
-            $help()                 Show help
-                                               ")
+                                               cat("        [HELP] PicSureHpdsLib::PicSureHpdsBypassConnection(url, token)\n")
+                                               cat("            $list()                 List resources\n")
+                                               cat("            $help()                 Show help\n")
                                                invisible(self)
                                              }
                                            )
@@ -127,7 +154,12 @@ PicSureHpdsBypassConnectionAPI <- R6::R6Class("PicSureHpdsBypassConnectionAPI",
                                               lock_objects = FALSE,
                                               public = list(
                                                 initialize = function(arg_url, arg_token) {
-                                                  self$url <- arg_url
+                                                  # trim and make sure URL ends in "/"
+                                                  endpoint <- str_trim(arg_url)
+                                                  if (str_detect(endpoint, "/$") == FALSE) {
+                                                    endpoint <- paste(endpoint, "/", sep="")
+                                                  }
+                                                  self$url <- endpoint
                                                   self$token <- arg_token
                                                 },
                                                 info = function(resource_uuid) {
@@ -151,11 +183,7 @@ PicSureHpdsBypassConnectionAPI <- R6::R6Class("PicSureHpdsBypassConnectionAPI",
                                                 },
                                                 asynchQuery = function(resource_uuid, query) { writeLines(c(resource_uuid, query)) },
                                                 synchQuery = function(resource_uuid, query) {
-                                                  if (resource_uuid == FALSE) {
-                                                    full_url = paste(self$url, "query/sync/", sep="")
-                                                  } else {
-                                                    full_url = paste(self$url, "query/sync/", resource_uuid, sep="")
-                                                  }
+                                                  full_url = paste(self$url, "query/sync/", sep="")
                                                   if (query == FALSE) {
                                                     query <- list()
                                                     query$query <- ""
